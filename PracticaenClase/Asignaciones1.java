9532bc27d1116b9b295bb207dc5d5a75cf34d857
Thony11 <thonyceli11@gmail.com>
2019-10-24 22:35:29 -0500

    Practicas en Clase

---
+++ PracticaenClase/Asignaciones1.java
@@ -0,0 +1,10 @@
+public class Asignaciones1 {
+	public static void main(String[] args) {
+		int x = 2;
+		int y = 9;
+		int sum = x + y;
+		int mul = x * y;
+		System.out.println("La suma de variables es " + sum);
+		System.out.println("El producto de mis variables es " + mul);
+	}
+}		
